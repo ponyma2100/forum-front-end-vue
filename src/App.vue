e2c910efd360a719dd53f14b4dacd15032192214
Pony <ponyma2100@gmail.com>
2020-12-17 02:17:15 +0800

    feat: project init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="restaurant-forum">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default {
+  name: "App",
+};
+</script>
