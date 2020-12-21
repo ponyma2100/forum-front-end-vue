c82feb08c4a4036ccf292b5a703e49aee25d0db8
Pony <ponyma2100@gmail.com>
2020-12-22 00:33:36 +0800

    feat: add previousPage and nextPage

--- src/components/RestaurantPagination.vue
+++ src/components/RestaurantPagination.vue
@@ -2,34 +2,49 @@
   <nav aria-label="Page navigation example">
     <ul class="pagination">
       <!-- 前一頁 previousPage -->
-      <li class="page-item">
-        <a class="page-link" aria-label="Previous" href="#">
+      <li
+        v-show="previousPage"
+        :class="['page-item', { disabled: currentPage === 1 }]"
+      >
+        <router-link
+          class="page-link"
+          aria-label="Previous"
+          :to="{
+            name: 'restaurants',
+            query: { categoryId, page: previousPage },
+          }"
+        >
           <span aria-hidden="true">&laquo;</span>
-        </a>
+        </router-link>
       </li>
 
       <li
-        class="page-item"
         v-for="page in totalPage"
         :key="page"
         :class="['page-item', { active: currentPage === page }]"
       >
         <router-link
           class="page-link"
-          :to="{
-            name: 'restaurants',
-            query: { categoryId, page },
-          }"
+          :to="{ name: 'restaurants', query: { categoryId, page } }"
         >
           {{ page }}
         </router-link>
       </li>
 
       <!-- 後一頁 nextPage -->
-      <li class="page-item">
-        <a class="page-link" aria-label="Next" href="#">
+      <li
+        :class="['page-item', { disabled: currentPage === totalPage.length }]"
+      >
+        <router-link
+          class="page-link"
+          aria-label="Next"
+          :to="{
+            name: 'restaurants',
+            query: { categoryId, page: nextPage },
+          }"
+        >
           <span aria-hidden="true">&raquo;</span>
-        </a>
+        </router-link>
       </li>
     </ul>
   </nav>
