c8a046cca4eb9d07c970c59011307af8654dfafb
Pony <ponyma2100@gmail.com>
2020-12-23 03:25:09 +0800

    feat: add Restaurant page

---
+++ src/components/RestaurantDetail.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="row">
+    <div class="col-md-12 mb-3">
+      <h1>{{ restaurant.name }}</h1>
+      <p class="badge badge-secondary mt-1 mb-3">
+        {{ restaurant.categoryName }}
+      </p>
+    </div>
+    <div class="col-lg-4">
+      <img
+        class="img-responsive center-block"
+        :src="restaurant.image"
+        style="width: 250px; margin-bottom: 25px"
+      />
+      <div class="contact-info-wrap">
+        <ul class="list-unstyled">
+          <li>
+            <strong>Opening Hour:</strong>
+            {{ restaurant.openingHours }}
+          </li>
+          <li>
+            <strong>Tel:</strong>
+            {{ restaurant.tel }}
+          </li>
+          <li>
+            <strong>Address:</strong>
+            {{ restaurant.address }}
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-lg-8">
+      <p>{{ restaurant.description }}</p>
+      <a class="btn btn-primary btn-border mr-2" href="#">Dashboard</a>
+
+      <button
+        v-if="restaurant.isFavorited"
+        @click.stop.prevent="deleteFavorited"
+        type="button"
+        class="btn btn-danger btn-border mr-2"
+      >
+        移除最愛
+      </button>
+      <button
+        v-else
+        @click.stop.prevent="addFavorited"
+        type="button"
+        class="btn btn-primary btn-border mr-2"
+      >
+        加到最愛
+      </button>
+      <button
+        v-if="restaurant.isLiked"
+        @click.stop.prevent="deleteLiked"
+        type="button"
+        class="btn btn-danger like mr-2"
+      >
+        Unlike
+      </button>
+      <button
+        v-else
+        @click.stop.prevent="addLiked"
+        type="button"
+        class="btn btn-primary like mr-2"
+      >
+        Like
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      restaurant: this.initialRestaurant,
+    };
+  },
+  props: {
+    initialRestaurant: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    addFavorited() {
+      this.restaurant = {
+        ...this.restaurant,
+        isFavorited: true,
+      };
+    },
+    deleteFavorited() {
+      this.restaurant = {
+        ...this.restaurant,
+        isFavorited: false,
+      };
+    },
+    addLiked() {
+      this.restaurant = {
+        ...this.restaurant,
+        isLiked: true,
+      };
+    },
+    deleteLiked() {
+      this.restaurant = {
+        ...this.restaurant,
+        isLiked: false,
+      };
+    },
+  },
+};
+</script>
---
+++ src/views/Restaurant.vue
@@ -0,0 +1,232 @@
+<template>
+  <div class="container py-5">
+    <h1>餐廳描述頁</h1>
+    <!-- 餐廳資訊頁 RestaurantDetail -->
+    <RestaurantDetail :initial-restaurant="restaurant" />
+    <hr />
+    <!-- 餐廳評論 RestaurantComments -->
+    <RestaurantComments :restaurant-comments="restaurantComments" />
+    <!-- 新增評論 CreateComment -->
+  </div>
+</template>
+
+
+<script>
+import RestaurantDetail from "./../components/RestaurantDetail";
+import RestaurantComments from "./../components/RestaurantComments";
+
+const dummyData = {
+  restaurant: {
+    id: 1,
+    name: "Loren Gulgowski",
+    tel: "695.115.9032",
+    address: "497 Abbott Estates",
+    opening_hours: "08:00",
+    description: "inventore",
+    image:
+      "https://loremflickr.com/320/240/restaurant,food/?random=9.385535305221993",
+    viewCounts: 30,
+    createdAt: "2020-12-15T06:35:43.000Z",
+    updatedAt: "2020-12-22T10:45:43.044Z",
+    CategoryId: 5,
+    Category: {
+      id: 5,
+      name: "素食料理",
+      createdAt: "2020-12-15T06:35:43.000Z",
+      updatedAt: "2020-12-15T06:35:43.000Z",
+    },
+    FavoritedUsers: [
+      {
+        id: 2,
+        name: "user1",
+        email: "user1@example.com",
+        password:
+          "$2a$10$m11qLlDOol1b3XCa393Bwe.hW4mt/6DS.mUsgFtati5LW4BbX81EG",
+        isAdmin: false,
+        image: null,
+        createdAt: "2020-12-15T06:35:43.000Z",
+        updatedAt: "2020-12-20T01:32:03.000Z",
+        Favorite: {
+          UserId: 2,
+          RestaurantId: 1,
+          createdAt: "2020-03-07T14:43:52.000Z",
+          updatedAt: "2020-03-07T14:43:52.000Z",
+        },
+      },
+      {
+        id: 1,
+        name: "root",
+        email: "root@example.com",
+        password:
+          "$2a$10$jBS/Y4.hceDXkEC5y9ZGne81Y7i5wNwNcy6wAKjNdBykCzlEfWmLm",
+        isAdmin: true,
+        image: null,
+        createdAt: "2020-12-15T06:35:43.000Z",
+        updatedAt: "2020-12-15T06:35:43.000Z",
+        Favorite: {
+          UserId: 1,
+          RestaurantId: 1,
+          createdAt: "2020-12-22T06:54:16.000Z",
+          updatedAt: "2020-12-22T06:54:16.000Z",
+        },
+      },
+    ],
+    LikedUsers: [
+      {
+        id: 2,
+        name: "user1",
+        email: "user1@example.com",
+        password:
+          "$2a$10$m11qLlDOol1b3XCa393Bwe.hW4mt/6DS.mUsgFtati5LW4BbX81EG",
+        isAdmin: false,
+        image: null,
+        createdAt: "2020-12-15T06:35:43.000Z",
+        updatedAt: "2020-12-20T01:32:03.000Z",
+        Like: {
+          UserId: 2,
+          RestaurantId: 1,
+          createdAt: "2020-03-07T14:42:45.000Z",
+          updatedAt: "2020-03-07T14:42:45.000Z",
+        },
+      },
+      {
+        id: 1,
+        name: "root",
+        email: "root@example.com",
+        password:
+          "$2a$10$jBS/Y4.hceDXkEC5y9ZGne81Y7i5wNwNcy6wAKjNdBykCzlEfWmLm",
+        isAdmin: true,
+        image: null,
+        createdAt: "2020-12-15T06:35:43.000Z",
+        updatedAt: "2020-12-15T06:35:43.000Z",
+        Like: {
+          UserId: 1,
+          RestaurantId: 1,
+          createdAt: "2020-12-22T06:55:31.000Z",
+          updatedAt: "2020-12-22T06:55:31.000Z",
+        },
+      },
+    ],
+    Comments: [
+      {
+        id: 1,
+        text: "Placeat aut molestiae incidunt repudiandae modi nulla a in.",
+        UserId: 2,
+        RestaurantId: 1,
+        createdAt: "2020-12-15T06:35:43.000Z",
+        updatedAt: "2020-12-15T06:35:43.000Z",
+        User: {
+          id: 2,
+          name: "user1",
+          email: "user1@example.com",
+          password:
+            "$2a$10$m11qLlDOol1b3XCa393Bwe.hW4mt/6DS.mUsgFtati5LW4BbX81EG",
+          isAdmin: false,
+          image: null,
+          createdAt: "2020-12-15T06:35:43.000Z",
+          updatedAt: "2020-12-20T01:32:03.000Z",
+        },
+      },
+      {
+        id: 51,
+        text: "Et aut molestiae autem.",
+        UserId: 1,
+        RestaurantId: 1,
+        createdAt: "2020-12-15T06:35:43.000Z",
+        updatedAt: "2020-12-15T06:35:43.000Z",
+        User: {
+          id: 1,
+          name: "root",
+          email: "root@example.com",
+          password:
+            "$2a$10$jBS/Y4.hceDXkEC5y9ZGne81Y7i5wNwNcy6wAKjNdBykCzlEfWmLm",
+          isAdmin: true,
+          image: null,
+          createdAt: "2020-12-15T06:35:43.000Z",
+          updatedAt: "2020-12-15T06:35:43.000Z",
+        },
+      },
+      {
+        id: 101,
+        text: "A et magni quidem in et magnam.",
+        UserId: 2,
+        RestaurantId: 1,
+        createdAt: "2020-12-15T06:35:43.000Z",
+        updatedAt: "2020-12-15T06:35:43.000Z",
+        User: {
+          id: 2,
+          name: "user1",
+          email: "user1@example.com",
+          password:
+            "$2a$10$m11qLlDOol1b3XCa393Bwe.hW4mt/6DS.mUsgFtati5LW4BbX81EG",
+          isAdmin: false,
+          image: null,
+          createdAt: "2020-12-15T06:35:43.000Z",
+          updatedAt: "2020-12-20T01:32:03.000Z",
+        },
+      },
+    ],
+  },
+  isFavorited: true,
+  isLiked: false,
+};
+
+export default {
+  components: {
+    RestaurantDetail,
+    RestaurantComments,
+  },
+
+  data() {
+    return {
+      restaurant: {
+        id: -1,
+        name: "",
+        tel: "",
+        address: "",
+        opening_hours: "",
+        description: "",
+        image: "",
+        categoryName: "",
+      },
+      isLiked: false,
+      isFavorited: false,
+      restaurantComments: [],
+    };
+  },
+  created() {
+    const { id: restaurantId } = this.$route.params;
+    this.fetchRestaurant(restaurantId);
+  },
+  methods: {
+    fetchRestaurant(restaurantId) {
+      console.log("fetchRestaurant id: ", restaurantId);
+      const { restaurant, isFavorited, isLiked } = dummyData;
+      const {
+        id,
+        name,
+        tel,
+        address,
+        opening_hours: openingHours,
+        description,
+        image,
+        Category,
+        Comments,
+      } = restaurant;
+      this.restaurant = {
+        id,
+        name,
+        tel,
+        address,
+        openingHours,
+        description,
+        image,
+        categoryName: Category.name,
+        isFavorited,
+        isLiked,
+      };
+      this.restaurantComments = Comments;
+    },
+  },
+};
+</script>
