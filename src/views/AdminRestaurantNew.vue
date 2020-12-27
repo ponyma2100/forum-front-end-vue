a31bd6c3ee165edb8b56e8af6bf44404000ab189
Pony <ponyma2100@gmail.com>
2020-12-28 03:26:24 +0800

    feat: add AdminRestaurantForm

--- src/views/AdminRestaurantNew.vue
+++ src/views/AdminRestaurantNew.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container py-5">
     <!-- 餐廳表單 AdminRestaurantForm -->
-    <AdminRestaurantNew />
+    <AdminRestaurantNew @after-submit="handleAfterSubmit" />
   </div>
 </template>
 
@@ -11,6 +11,14 @@ import AdminRestaurantNew from "./../components/AdminRestaurantForm";
 export default {
   components: {
     AdminRestaurantNew,
+  },
+  methods: {
+    handleAfterSubmit(formData) {
+      // TODO:  透過API 將表單資料送到伺服器
+      for (let [name, value] of formData.entries()) {
+        console.log(name + ":" + value);
+      }
+    },
   },
 };
 </script>
