1fe58427e772b3fcd4479f9fff556e07ece8fb80
Pony <ponyma2100@gmail.com>
2020-12-27 23:45:51 +0800

    feat: add AdminRestaurantNew and AdminRestaurantForm

---
+++ src/views/AdminRestaurantNew.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container py-5">
+    <!-- 餐廳表單 AdminRestaurantForm -->
+    <AdminRestaurantNew />
+  </div>
+</template>
+
+<script>
+import AdminRestaurantNew from "./../components/AdminRestaurantForm";
+
+export default {
+  components: {
+    AdminRestaurantNew,
+  },
+};
+</script>
