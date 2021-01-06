463dd395cbdc198378445d8815c480f28b1384e1
Pony <ponyma2100@gmail.com>
2021-01-07 00:52:53 +0800

    fix: integrate admin.restaurants.getDetail in AdminRestaurant

--- src/views/AdminRestaurantEdit.vue
+++ src/views/AdminRestaurantEdit.vue
@@ -10,29 +10,8 @@
 
 <script>
 import AdminRestaurantForm from "./../components/AdminRestaurantForm";
-
-const dummyData = {
-  restaurant: {
-    id: 1,
-    name: "Laurence Reynolds",
-    tel: "1-657-067-3756 x9782",
-    address: "187 Kirlin Squares",
-    opening_hours: "08:00",
-    description: "sit est mollitia",
-    image:
-      "https://loremflickr.com/320/240/restaurant,food/?random=91.29816290184887",
-    viewCounts: 1,
-    createdAt: "2019-07-30T16:24:55.432Z",
-    updatedAt: "2019-07-30T17:26:43.260Z",
-    CategoryId: 3,
-    Category: {
-      id: 3,
-      name: "義大利料理",
-      createdAt: "2019-07-30T16:24:55.429Z",
-      updatedAt: "2019-07-30T16:24:55.429Z",
-    },
-  },
-};
+import adminAPI from "./../apis/admin";
+import { Toast } from "./../utils/helpers";
 
 export default {
   components: {
@@ -58,30 +37,40 @@ export default {
     this.fetchRestaurant(restaurantId);
   },
   methods: {
-    fetchRestaurant(restaurantId) {
-      // console.log("fetchRestaurant id:", restaurantId);
-      const { restaurant } = dummyData;
-      const {
-        id,
-        name,
-        tel,
-        address,
-        opening_hours: openingHours,
-        description,
-        image,
-        CategoryId: categoryId,
-      } = restaurant;
-      this.restaurant = {
-        ...this.restaurant,
-        id,
-        name,
-        tel,
-        address,
-        openingHours,
-        description,
-        image,
-        categoryId,
-      };
+    async fetchRestaurant(restaurantId) {
+      try {
+        const { data } = await adminAPI.restaurants.getDetail({ restaurantId });
+        console.log(data);
+        //透過解構賦值將需要的資料取出
+        const {
+          id,
+          name,
+          tel,
+          address,
+          opening_hours: openingHours,
+          description,
+          image,
+          CategoryId: categoryId,
+        } = data.restaurant;
+        //  將資料帶入 Vue 內
+        this.restaurant = {
+          ...this.restaurant,
+          id,
+          name,
+          tel,
+          address,
+          openingHours,
+          description,
+          image,
+          categoryId,
+        };
+      } catch (error) {
+        console.log("error", error);
+        Toast.fire({
+          icon: "error",
+          title: "無法取得餐廳資料，請稍後再試",
+        });
+      }
     },
     handleAfterSubmit(formData) {
       // TODO:  透過API 將表單資料送到伺服器
@@ -91,4 +80,26 @@ export default {
     },
   },
 };
+// const dummyData = {
+//   restaurant: {
+//     id: 1,
+//     name: "Laurence Reynolds",
+//     tel: "1-657-067-3756 x9782",
+//     address: "187 Kirlin Squares",
+//     opening_hours: "08:00",
+//     description: "sit est mollitia",
+//     image:
+//       "https://loremflickr.com/320/240/restaurant,food/?random=91.29816290184887",
+//     viewCounts: 1,
+//     createdAt: "2019-07-30T16:24:55.432Z",
+//     updatedAt: "2019-07-30T17:26:43.260Z",
+//     CategoryId: 3,
+//     Category: {
+//       id: 3,
+//       name: "義大利料理",
+//       createdAt: "2019-07-30T16:24:55.429Z",
+//       updatedAt: "2019-07-30T16:24:55.429Z",
+//     },
+//   },
+// };
 </script>
