4edb2617214b70bb9130cc6cf4db4751519c350d
Pony <ponyma2100@gmail.com>
2021-01-10 23:29:37 +0800

    fix: use Spinner in RestaurantsFeeds

--- src/views/RestaurantsFeeds.vue
+++ src/views/RestaurantsFeeds.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="container py-5">
     <NavTabs />
-    <h1 class="mt-5">最新動態</h1>
-    <div class="row">
-      <div class="col-md-6">
-        <h3>最新餐廳</h3>
-        <!-- 最新餐廳 NewestRestaurants -->
-        <NewestRestaurants :restaurants="restaurants" />
+    <Spinner v-if="isLoading" />
+    <template v-else>
+      <h1 class="mt-5">最新動態</h1>
+      <div class="row">
+        <div class="col-md-6">
+          <h3>最新餐廳</h3>
+          <!-- 最新餐廳 NewestRestaurants -->
+          <NewestRestaurants :restaurants="restaurants" />
+        </div>
+        <div class="col-md-6">
+          <!-- 最新評論 NewestComments-->
+          <h3>最新評論</h3>
+          <NewestComments :comments="comments" />
+        </div>
       </div>
-      <div class="col-md-6">
-        <!-- 最新評論 NewestComments-->
-        <h3>最新評論</h3>
-        <NewestComments :comments="comments" />
-      </div>
-    </div>
+    </template>
   </div>
 </template>
 
@@ -21,6 +24,7 @@
 import NavTabs from "./../components/NavTabs";
 import NewestRestaurants from "./../components/NewestRestaurants";
 import NewestComments from "./../components/NewestComments";
+import Spinner from "./../components/Spinner";
 import restaurantsAPI from "./../apis/restaurants";
 import { Toast } from "./../utils/helpers";
 
@@ -30,11 +34,13 @@ export default {
     NavTabs,
     NewestRestaurants,
     NewestComments,
+    Spinner,
   },
   data() {
     return {
       restaurants: [],
       comments: [],
+      isLoading: true,
     };
   },
   created() {
@@ -43,12 +49,15 @@ export default {
   methods: {
     async fetchFeeds() {
       try {
+        this.isLoading = true;
         const { data } = await restaurantsAPI.getRestaurantsFeeds();
         this.restaurants = data.restaurants;
         this.comments = data.comments.filter(
           (comment) => comment.Restaurant && comment.text
         );
+        this.isLoading = false;
       } catch (error) {
+        this.isLoading = false;
         console.log("error", error);
         Toast.fire({
           icon: "error",
