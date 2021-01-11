80c07d8bdf6829740c369512ffd24738312c2f2a
Pony <ponyma2100@gmail.com>
2021-01-12 03:08:07 +0800

    Add UserProfileCard

---
+++ src/components/UserProfileCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="card mb-3">
+    <div class="row no-gutters">
+      <div class="col-md-4">
+        <img :src="user.image" width="300px" height="300px" />
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">{{ user.name }}</h5>
+          <p class="card-text">{{ user.email }}</p>
+          <ul class="list-unstyled list-inline">
+            <li>
+              <strong>{{ user.commentsLength }}</strong> 已評論餐廳
+            </li>
+            <li>
+              <strong>{{ user.favoritedRestaurantsLenth }}</strong> 收藏的餐廳
+            </li>
+            <li>
+              <strong>{{ user.followingsLength }}</strong> followings (追蹤者)
+            </li>
+            <li>
+              <strong>{{ user.followersLength }}</strong> followers (追隨者)
+            </li>
+          </ul>
+          <template v-if="isCurrentUser">
+            <a href="/users/1/edit"
+              ><button type="submit" class="btn btn-primary">edit</button></a
+            >
+          </template>
+          <template v-else>
+            <button
+              @click.stop.prevent="addFollowing"
+              type="button"
+              class="btn btn-danger"
+            >
+              取消追蹤
+            </button>
+            <button type="button" class="btn btn-primary">追蹤</button>
+          </template>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+    isCurrentUser: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
+</script>
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="container py-5">
     <!-- UserProfileCard -->
+    <UserProfileCard
+      :user="user"
+      :is-current-user="currentUser.id === user.id"
+    />
     <!-- UserFollowingsCard -->
     <!-- UserFollowersCard -->
     <!-- UserCommentsCard -->
@@ -10,22 +14,28 @@
 
 <script>
 import userAPI from "./../apis/users";
-
+import UserProfileCard from "./../components/UserProfileCard";
+import { mapState } from "vuex";
 export default {
+  components: {
+    UserProfileCard,
+  },
   data() {
     return {
       user: {
         id: -1,
         image: "",
         name: "",
+        email: "",
         followingsLength: 0,
         followersLength: 0,
         commentsLength: 0,
-        favoritedRestaurantsLenth: 0,
+        favoritedRestaurantsLength: 0,
       },
       isFollowed: false,
       followings: [],
       followers: [],
+      Comments: [],
       favoritedRestaurants: [],
     };
   },
@@ -33,11 +43,36 @@ export default {
     const { id } = this.$route.params;
     this.fetchUser(id);
   },
+  computed: {
+    ...mapState(["currentUser"]),
+  },
   methods: {
     async fetchUser(userId) {
       try {
         const { data } = await userAPI.get({ userId });
         console.log(data);
+        const {
+          id,
+          image,
+          name,
+          email,
+          Comments,
+          FavoritedRestaurants,
+          Followers,
+          Followings,
+        } = data.profile;
+
+        this.user = {
+          ...this.user,
+          id,
+          image,
+          name,
+          email,
+          followingsLength: Followings.length,
+          followersLength: Followers.length,
+          commentsLength: Comments.length,
+          favoritedRestaurantsLength: FavoritedRestaurants.length,
+        };
       } catch (error) {
         console.log(error);
       }
